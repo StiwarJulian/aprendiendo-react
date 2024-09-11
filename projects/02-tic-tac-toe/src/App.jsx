1dd61baf97acb33d88a6b54a42582bd9f1205c9f
Stiwar Julian <stiwarospina6@gmail.com>
2024-09-11 18:29:42 -0500

    Extract to component tic tac toe

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,36 +1,11 @@
 import { useState } from 'react'
+import { Square } from './components/Square.jsx'
+import { TURNS } from './constants.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
 import confetti from 'canvas-confetti'
-
-const TURNS = {
-  X: 'x',
-  O: 'o',
-}
-
-// eslint-disable-next-line react/prop-types
-const Square = ({ children, isSelected, updateBoard, index}) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBINATIONS = [
-  [0, 1, 2], // top row
-  [3, 4, 5], // middle row
-  [6, 7, 8], // bottom row
-  [0, 3, 6], // left column
-  [1, 4, 7], // middle column
-  [2, 5, 8], // right column
-  [0, 4, 8], // diagonal 1
-  [2, 4, 6], // diagonal 2
-]
+import { WinnerModal } from './components/WinnerModal.jsx'
+import { Board } from './components/Board.jsx'
+import { TurnSelected } from './components/TurnSelected.jsx'
 
 function App() {
   const [board, setBoard] = useState(
@@ -39,23 +14,6 @@ function App() {
 
   const [turn, setTurn] = useState(TURNS.X)
   const [winner, setWinner] = useState(null)
-
-  const checkWinner = (boardToCheck) => {
-    for (const combination of WINNER_COMBINATIONS) {
-      const [a, b, c] = combination
-      if (
-        boardToCheck[a] && 
-        boardToCheck[a] === boardToCheck[b] && 
-        boardToCheck[a] === boardToCheck[c]) {
-        return boardToCheck[a]
-      }
-    }
-    return null
-  }
-  
-  const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square !== null)
-  }
 
   const updateBoard = (index) => {
     if (board[index] !== null || winner) return
@@ -67,7 +25,7 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       confetti()
       setWinner(newWinner)
@@ -86,46 +44,12 @@ function App() {
     <main className="board">
       <h1>Tic tac toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
-      <section className="game">
-        {board.map((square, index) => {
-          return (
-            <Square 
-              key={index}
-              index={index}
-              updateBoard={updateBoard}
-            >
-              {square}
-            </Square>
-          )
-        })}
-      </section>
 
-      <section className='turn'>
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square>
-      </section>
+      <Board board={board} updateBoard={updateBoard} />
 
-      {
-        winner !== null && (
-          <section className='winner'>
-            <div className='text'>
-              <h2>{ winner === false ? 'Empate' : 'Gano: '}</h2>
+      <TurnSelected turn={turn}/>
 
-              <header className='win'>
-                {winner && <Square>{winner}</Square>}
-              </header>
-
-              <footer>
-                <button onClick={resetGame}>Empezar De Nuevo</button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
+      <WinnerModal winner={winner} resetGame={resetGame} />
     </main>
   )
 }
---
+++ projects/02-tic-tac-toe/src/components/TurnSelected.jsx
@@ -0,0 +1,16 @@
+import { TURNS } from "../constants";
+import { Square } from "./Square";
+
+// eslint-disable-next-line react/prop-types
+export function TurnSelected( { turn } ) {
+    return (
+    	<section className='turn'>
+				<Square isSelected={turn === TURNS.X}>
+					{TURNS.X}
+				</Square>
+				<Square isSelected={turn === TURNS.O}>
+    			{TURNS.O}
+        </Square>
+      </section>
+    )
+}
